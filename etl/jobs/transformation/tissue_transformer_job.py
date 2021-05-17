a3ae0964151b910b088eb867f549b9331430419c
Mauricio Martinez <mauroz77@gmail.com>
2021-05-17 09:22:29 +0100

    Create Transformation task for Tissue
    - Fix error. Get the correct data from sample module: collection_site instead tumour_type

--- etl/jobs/transformation/tissue_transformer_job.py
+++ etl/jobs/transformation/tissue_transformer_job.py
@@ -25,17 +25,15 @@ def main(argv):
 
 
 def transform_tissue(raw_sample_df: DataFrame) -> DataFrame:
-    tissue_df = get_tumour_type_from(raw_sample_df).union(get_primary_type_from_sample(raw_sample_df))
+    tissue_df = get_tumour_type_from_collection_site(raw_sample_df).union(get_primary_type_from_sample(raw_sample_df))
     tissue_df = tissue_df.drop_duplicates()
     tissue_df = add_id(tissue_df, "id")
     tissue_df = tissue_df.select("id", "name")
-    print("tissue_df")
-    tissue_df.show()
     return tissue_df
 
 
-def get_tumour_type_from(raw_sample_df: DataFrame) -> DataFrame:
-    return raw_sample_df.select(init_cap_and_trim_all("tumour_type").alias("name"))
+def get_tumour_type_from_collection_site(raw_sample_df: DataFrame) -> DataFrame:
+    return raw_sample_df.select(init_cap_and_trim_all("collection_site").alias("name"))
 
 
 def get_primary_type_from_sample(raw_sample_df: DataFrame) -> DataFrame:
