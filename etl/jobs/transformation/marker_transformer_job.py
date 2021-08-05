33fa6e4e48e61f129b0b8d64b1ab79cbb272ff3a
CsabaHalmagyi <csaba@ebi.ac.uk>
2021-08-05 10:51:43 +0100

    Extend marker_transformer_job to create aliases and previous symbols

--- etl/jobs/transformation/marker_transformer_job.py
+++ etl/jobs/transformation/marker_transformer_job.py
@@ -15,12 +15,19 @@ def main(argv):
     output_path = argv[2]
 
     spark = SparkSession.builder.getOrCreate()
-    markers_df = biomarker_extractor.get_marker_dataframe(input_path)
+    markers_list = biomarker_extractor.extract_markers(input_path)
+    markers_df = biomarker_extractor.create_marker_dataframe(markers_list)
     markers_df = add_id(markers_df, "id")
     markers_df.write.mode("overwrite").parquet(output_path)
+
+    marker_aliases_df = biomarker_extractor.create_marker_aliases_dataframe(markers_list)
+    marker_aliases_df = add_id(marker_aliases_df, "id")
+    marker_aliases_df.write.mode("overwrite").parquet(output_path + "/aliases")
+
+    marker_prev_symbols_df = biomarker_extractor.create_marker_prev_symbols_dataframe(markers_list)
+    marker_prev_symbols_df = add_id(marker_prev_symbols_df, "id")
+    marker_prev_symbols_df.write.mode("overwrite").parquet(output_path + "/prev_symbols")
 
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
-
-
