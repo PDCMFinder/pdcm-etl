f3496021bc1165073bfa6a3100e3868c124f2e5b
CsabaHalmagyi <csaba@ebi.ac.uk>
2021-09-15 15:54:39 +0100

    Gene marker ETL initial commit

--- etl/constants.py
+++ etl/constants.py
@@ -20,6 +20,7 @@ class Constants:
     MOLECULAR_DATA_SAMPLE_MODULE = "molchar_sample"
     MOLECULAR_DATA_PLATFORM_MODULE = "molchar_platform"
     MOLECULAR_DATA_PLATFORM_WEB_MODULE = "molchar_platform_web"
+    GENE_MARKER_MODULE = "markers"
 
     DATA_SOURCE_COLUMN = "data_source_tmp"
 
@@ -58,3 +59,4 @@ class Constants:
     EXPRESSION_MOLECULAR_DATA_ENTITY = "expression_molecular_data"
     MUTATION_MARKER_ENTITY = "mutation_marker"
     MUTATION_MEASUREMENT_DATA_ENTITY = "mutation_measurement_data"
+    GENE_MARKER_ENTITY = "gene_marker"
