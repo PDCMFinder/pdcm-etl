022a42b6c36fcef2665d0a239800ef9d3f91034b
CsabaHalmagyi <csaba@ebi.ac.uk>
2021-10-12 13:21:43 +0100

    Replace Malignant X Neoplasms with X Cancer in term name

--- etl/jobs/transformation/ontology_term_diagnosis_transformer_job.py
+++ etl/jobs/transformation/ontology_term_diagnosis_transformer_job.py
@@ -2,7 +2,8 @@ import sys
 import networkx as nx
 from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.functions import col
-
+from pyspark.sql.functions import regexp_replace
+from pyspark.sql.functions import trim
 from etl.jobs.util.id_assigner import add_id
 from etl.jobs.util.graph_builder import *
 
@@ -36,7 +37,17 @@ def transform_ontology_term_diagnosis(ontology_term_df: DataFrame) -> DataFrame:
     cancer_term_id_list = get_term_ids_from_graph(cancer_graph)
     print("Cancer terms:" + str(len(cancer_term_id_list)))
     ontology_term_diagnosis_df = ontology_term_df.where(col("term_id").isin(cancer_term_id_list))
+    ontology_term_diagnosis_df = update_term_labels(ontology_term_diagnosis_df)
     ontology_term_diagnosis_df = add_id(ontology_term_diagnosis_df, "id")
+
+    return ontology_term_diagnosis_df
+
+
+def update_term_labels(ontology_term_diagnosis_df: DataFrame) -> DataFrame:
+    ontology_term_diagnosis_df = ontology_term_diagnosis_df.withColumn('term_name', regexp_replace('term_name',
+                                                                                                   '(.*)Malignant(.*)Neoplasm(.*)',
+                                                                                                   '$1$2Cancer$3')).withColumn(
+        'term_name', trim(col('term_name')))
     return ontology_term_diagnosis_df
 
 
