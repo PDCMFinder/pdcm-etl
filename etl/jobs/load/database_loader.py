580a33d01aaeba93ca5af127a1fdc6a05ce05416
Federico López Gómez <fico89@gmail.com>
2021-10-18 20:41:32 +0100

    Optimize DB load on cluster

--- etl/jobs/load/database_loader.py
+++ etl/jobs/load/database_loader.py
@@ -13,13 +13,11 @@ def main(argv):
     db_host = argv[3]
     db_port = argv[4]
     db_name = argv[5]
-    input_parquet_paths = argv[6].split("|")
-    table_names = argv[7].split("|")
+    input_parquet_path = argv[6]
+    table_name = argv[7]
     output_path = argv[8]
 
-    api_dataframes = [
-        spark.read.parquet(input_path) for input_path in input_parquet_paths
-    ]
+    api_dataframe = spark.read.parquet(input_parquet_path)
     properties = {
         "user": db_user,
         "password": db_password,
@@ -28,17 +26,14 @@ def main(argv):
     }
     jdbc_url = f"jdbc:postgresql://{db_host}:{db_port}/{db_name}"
 
-    table_dataframe_map = zip(table_names, api_dataframes)
-
-    for table_name, dataframe in table_dataframe_map:
-        dataframe = dataframe.drop(Constants.DATA_SOURCE_COLUMN)
-        columns = get_columns_by_entity_name(table_name)
-        dataframe = dataframe.select(columns)
-        dataframe.write.option("truncate", "true").mode("overwrite").jdbc(
-            jdbc_url,
-            table_name,
-            properties=properties,
-        )
+    api_dataframe = api_dataframe.drop(Constants.DATA_SOURCE_COLUMN)
+    columns = get_columns_by_entity_name(table_name)
+    api_dataframe = api_dataframe.select(columns)
+    api_dataframe.write.option("truncate", "true").mode("overwrite").jdbc(
+        jdbc_url,
+        table_name,
+        properties=properties,
+    )
     spark.sparkContext.emptyRDD().saveAsTextFile(output_path)
 
 
