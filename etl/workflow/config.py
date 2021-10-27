d151b3e6b8ad5f0122423efb635a16bf93f9259e
Mauricio Martinez <mauroz77@gmail.com>
2021-10-27 10:51:58 +0100

    Processing time by task
    - Add a method that tells all luigi tasks to print the time it took to execute

--- etl/workflow/config.py
+++ etl/workflow/config.py
@@ -11,3 +11,13 @@ class PdcmConfig(luigi.Config):
             if self.deploy_mode in ["local", "client"]
             else WebHdfsTarget(path)
         )
+
+
+class TimeTaskMixin(object):
+    """
+    A mixin that will print out the tasks execution time to standard out, when the task is
+    finished
+    """
+    @luigi.Task.event_handler(luigi.Event.PROCESSING_TIME)
+    def processing_time(task, processing_time):
+        print('Processing time for task {0} was {1} seconds\n'.format(task, processing_time))
