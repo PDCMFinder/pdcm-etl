99a2553b757b3b350066035b3c563301f32374cb
Mauricio Martinez <mauroz77@gmail.com>
2022-02-02 16:40:47 +0000

    Tests for treatment_and_component_helper
    - Create tests for the different scenarios regarding treatment_name and treatment_dose
    - Change the call to the method that compares dataframes so it ignores the nullability of the columns when comparing schemas

--- etl/jobs/transformation/treatment_and_component_helper_transformer_job.py
+++ etl/jobs/transformation/treatment_and_component_helper_transformer_job.py
@@ -25,6 +25,7 @@ def main(argv):
 
 
 def transform_treatment_and_component_helper(treatment_protocol_df) -> DataFrame:
+    treatment_protocol_df = treatment_protocol_df.withColumnRenamed("id", "treatment_protocol_id")
     # Add split values for the columns treatment_name and treatment_dose
     df = add_split_values(treatment_protocol_df)
     df = df.withColumn("treatment_name_split_counter", size(col("treatment_name_split")))
@@ -35,22 +36,23 @@ def transform_treatment_and_component_helper(treatment_protocol_df) -> DataFrame
 
     # First case: the number of treatment names is the same as the number of treatment doses:
     matched_df = df_exploded_by_treatment_name.alias("a").join(
-        df_exploded_by_treatment_dose.alias("b"), on=["id", "pos", "count"])
+        df_exploded_by_treatment_dose.alias("b"), on=["treatment_protocol_id", "pos", "count"])
     matched_df = matched_df.select(
-        "a.id", "a.model_id", "a.patient_id", "a.single_treatment_name", "b.single_treatment_dose")
+        "a.treatment_protocol_id", "a.model_id", "a.patient_id", "a.single_treatment_name", "b.single_treatment_dose")
     matched_df = matched_df.withColumnRenamed("single_treatment_name", "treatment_name")
     matched_df = matched_df.withColumnRenamed("single_treatment_dose", "treatment_dose")
 
     # For any treatment and dose which don't match in cardinality, take the treatment and put as dose whatever the
     # original string for the dose was
-    df_exploded_by_treatment_name_unmatched = df_exploded_by_treatment_name.join(matched_df, ["id"], "leftanti")
+    df_exploded_by_treatment_name_unmatched = df_exploded_by_treatment_name.join(
+        matched_df, ["treatment_protocol_id"], "leftanti")
 
     # Avoid ambiguity in the columns after the join
     df = df.drop("model_id", "patient_id")
 
-    unmatched_df = df_exploded_by_treatment_name_unmatched.join(df, on=["id"])
+    unmatched_df = df_exploded_by_treatment_name_unmatched.join(df, on=["treatment_protocol_id"])
     unmatched_df = unmatched_df.select(
-        "id", "model_id", "patient_id", "single_treatment_name", "treatment_dose")
+        "treatment_protocol_id", "model_id", "patient_id", "single_treatment_name", "treatment_dose")
     unmatched_df = unmatched_df.withColumnRenamed("single_treatment_name", "treatment_name")
 
     # We might want to report the unmatched_df but for now we just use it as part of the results
@@ -59,7 +61,7 @@ def transform_treatment_and_component_helper(treatment_protocol_df) -> DataFrame
 
 def get_exploded_df_by_treatment_name(df: DataFrame) -> DataFrame:
     df_exploded_by_treatment_name = df.select(
-        "id", "model_id", "patient_id", "treatment_name", "treatment_name_split_counter",
+        "treatment_protocol_id", "model_id", "patient_id", "treatment_name", "treatment_name_split_counter",
         posexplode("treatment_name_split"), "response_id", "response_classification_id")
     df_exploded_by_treatment_name = df_exploded_by_treatment_name.withColumn("single_treatment_name", trim_all("col"))
     df_exploded_by_treatment_name = df_exploded_by_treatment_name.drop("col")
@@ -70,7 +72,7 @@ def get_exploded_df_by_treatment_name(df: DataFrame) -> DataFrame:
 
 def get_exploded_df_by_treatment_dose(df: DataFrame) -> DataFrame:
     df_exploded_by_treatment_dose = df.select(
-        "id", "model_id", "patient_id", "treatment_dose", "treatment_dose_split_counter",
+        "treatment_protocol_id", "model_id", "patient_id", "treatment_dose", "treatment_dose_split_counter",
         posexplode("treatment_dose_split"), "response_id", "response_classification_id")
     df_exploded_by_treatment_dose = df_exploded_by_treatment_dose.withColumn("single_treatment_dose", trim_all("col"))
     df_exploded_by_treatment_dose = df_exploded_by_treatment_dose.drop("col")
@@ -94,7 +96,7 @@ def add_counter_split_values(df: DataFrame) -> DataFrame:
 
 def get_expected_columns(model_drug_dosing_df: DataFrame) -> DataFrame:
     return model_drug_dosing_df.select(
-        "id",
+        "treatment_protocol_id",
         "model_id",
         "patient_id",
         "treatment_target",
--- tests/util.py
+++ tests/util.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 from pyspark.sql import DataFrame, SparkSession, Row
 from pyspark.sql.types import StructType, StructField, StringType
+from chispa.dataframe_comparer import *
 
 
 def convert_to_dataframe(spark: SparkSession, dict_list: List[Dict]) -> DataFrame:
@@ -9,7 +10,7 @@ def convert_to_dataframe(spark: SparkSession, dict_list: List[Dict]) -> DataFram
     return spark.createDataFrame([Row(**x) for x in dict_list], schema=schema)
 
 
-def dataframes_are_equal_ignore_id(df_a: DataFrame, df_b: DataFrame) -> bool:
+def assert_df_are_equal_ignore_id(df_a: DataFrame, df_b: DataFrame):
     df_a = df_a.drop("id")
     df_b = df_b.drop("id")
-    return df_a.columns == df_b.columns and df_a.subtract(df_b).count() == 0 and df_b.subtract(df_a).count() == 0
+    assert_df_equality(df_a, df_b, ignore_nullable=True)
