2770473521fe667c18bec3a7bc9bf41a363d4e58
Mauricio Martinez <mauroz77@gmail.com>
2022-03-11 10:46:35 +0000

    fix tests treatment-harmonisation

--- tests/etl/workflow/harmonisation/treatments_harmonisation/expected_outputs.py
+++ tests/etl/workflow/harmonisation/treatments_harmonisation/expected_outputs.py
@@ -1,7 +1,7 @@
 expected_harmonised_treatments = [
     {
         "protocol_model": "1",
-        "model_treatment_list": "ontology_t6(r2), ontology_t5(r2), ontology_r2",
-        "treatment_list": "ontology_t4(r1), ontology_t3(r1), ontology_t1, ontology_r1"
+        "model_treatment_list": "[ontology_t6(r2), ontology_t5(r2), ontology_r2]",
+        "treatment_list": "[ontology_t4(r1), ontology_t3(r1), ontology_t1, ontology_r1]"
     }
 ]
--- tests/etl/workflow/harmonisation/treatments_harmonisation/test_treatments_harmonisation.py
+++ tests/etl/workflow/harmonisation/treatments_harmonisation/test_treatments_harmonisation.py
@@ -2,7 +2,7 @@ from etl.jobs.transformation.harmonisation.treatments_harmonisation import harmo
 from tests.etl.workflow.harmonisation.treatments_harmonisation.expected_outputs import expected_harmonised_treatments
 from tests.etl.workflow.harmonisation.treatments_harmonisation.input_data import *
 from tests.util import convert_to_dataframe, assert_df_are_equal_ignore_id
-from pyspark.sql.functions import concat_ws, col
+from pyspark.sql.functions import col, array_sort
 
 
 def test_treatments_harmonisation(spark_session):
@@ -30,21 +30,13 @@ def test_treatments_harmonisation(spark_session):
         regimen_term_treatment_df)
 
     harmonised_treatments_df = harmonised_treatments_df.withColumn(
-        "model_treatment_list", concat_ws(", ", col("model_treatment_list")))
+        "model_treatment_list", array_sort(col("model_treatment_list")))
     harmonised_treatments_df = harmonised_treatments_df.withColumn(
-        "treatment_list", concat_ws(", ", col("treatment_list")))
-    # harmonised_treatments_dfeatments_df = harmonised_treatments_df.drop("model_treatment_list", "treatment_list")
-    # harmonised_treatments_df.withColumnRenamed("model_treatment_list_string")
+        "treatment_list", array_sort(col("treatment_list")))
 
     expected_df = convert_to_dataframe(spark_session, expected_harmonised_treatments)
-
-    print("obtained")
-    harmonised_treatments_df.show(truncate=False)
-    harmonised_treatments_df.printSchema()
-
-    print("expected")
-    expected_df.show(truncate=False)
-    expected_df.printSchema()
+    expected_df = expected_df.withColumn("model_treatment_list", array_sort(col("model_treatment_list")))
+    expected_df = expected_df.withColumn("treatment_list", array_sort(col("treatment_list")))
 
     assert_df_are_equal_ignore_id(harmonised_treatments_df, expected_df)
 
