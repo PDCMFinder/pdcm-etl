77a1417428d381cc54f082b46e57108176d685ae
Mauricio Martinez <mauroz77@gmail.com>
2022-09-02 15:22:58 +0100

    Assign fk to molecular_characterization for cell samples. Refactoring to centralise logic of assignation of fk to molecular_characterization for molecular data tables

--- etl/jobs/transformation/cna_molecular_data_transformer_job.py
+++ etl/jobs/transformation/cna_molecular_data_transformer_job.py
@@ -5,6 +5,7 @@ from pyspark.sql import DataFrame, SparkSession
 from etl.constants import Constants
 from etl.jobs.transformation.harmonisation.markers_harmonisation import harmonise_mutation_marker_symbols
 from etl.jobs.util.id_assigner import add_id
+from etl.jobs.util.molecular_characterization_fk_assigner import set_fk_molecular_characterization
 
 
 def main(argv):
@@ -33,7 +34,7 @@ def transform_cna_molecular_data(
         molecular_characterization_df: DataFrame, raw_cna_df: DataFrame,
         gene_markers_parquet_path: DataFrame) -> DataFrame:
     cna_df = get_cna_df(raw_cna_df)
-    cna_df = set_fk_molecular_characterization(cna_df, molecular_characterization_df)
+    cna_df = set_fk_molecular_characterization(cna_df, 'copy number alteration', molecular_characterization_df)
     cna_df = harmonise_mutation_marker_symbols(cna_df, gene_markers_parquet_path)
     cna_df = get_expected_columns(cna_df)
     cna_df = add_id(cna_df, "id")
@@ -55,30 +56,6 @@ def get_cna_df(raw_cna_df: DataFrame) -> DataFrame:
         "picnic_value",
         "ensembl_gene_id",
         "ncbi_gene_id").drop_duplicates()
-
-
-def set_fk_molecular_characterization(cna_df: DataFrame, molecular_characterization_df: DataFrame) -> DataFrame:
-    cna_df = cna_df.withColumnRenamed("platform_id", "platform_external_id")
-    molecular_characterization_df = molecular_characterization_df.withColumnRenamed(
-        "id", "molecular_characterization_id").where("molecular_characterisation_type = 'copy number alteration'")
-
-    molecular_characterization_df = molecular_characterization_df.select(
-        "molecular_characterization_id", "sample_origin", "external_patient_sample_id",
-        "external_xenograft_sample_id", "platform_external_id", Constants.DATA_SOURCE_COLUMN)
-
-    mol_char_patient_df = molecular_characterization_df.where("sample_origin = 'patient'")
-    mol_char_patient_df = mol_char_patient_df.withColumnRenamed("external_patient_sample_id", "sample_id")
-    cna_patient_sample_df = cna_df.join(
-        mol_char_patient_df, on=["sample_id", "platform_external_id", Constants.DATA_SOURCE_COLUMN], how='inner')
-
-    mol_char_xenograft_df = molecular_characterization_df.where("sample_origin = 'xenograft'")
-    mol_char_xenograft_df = mol_char_xenograft_df.withColumnRenamed("external_xenograft_sample_id", "sample_id")
-    cna_xenograft_sample_df = cna_df.join(
-        mol_char_xenograft_df, on=["sample_id", "platform_external_id", Constants.DATA_SOURCE_COLUMN], how='inner')
-
-    cna_df = cna_patient_sample_df.union(cna_xenograft_sample_df)
-    return cna_df
-
 
 def get_expected_columns(cna_df: DataFrame) -> DataFrame:
     return cna_df.select(
--- etl/jobs/transformation/expression_molecular_data_transformer_job.py
+++ etl/jobs/transformation/expression_molecular_data_transformer_job.py
@@ -5,6 +5,7 @@ from pyspark.sql import DataFrame, SparkSession
 from etl.constants import Constants
 from etl.jobs.transformation.harmonisation.markers_harmonisation import harmonise_mutation_marker_symbols
 from etl.jobs.util.id_assigner import add_id
+from etl.jobs.util.molecular_characterization_fk_assigner import set_fk_molecular_characterization
 
 
 def main(argv):
@@ -34,7 +35,7 @@ def transform_expression_molecular_data(
         raw_expression_df: DataFrame, gene_markers_parquet_path: DataFrame) -> DataFrame:
     expression_df = get_expression_df(raw_expression_df)
 
-    expression_df = set_fk_molecular_characterization(expression_df, molecular_characterization_df)
+    expression_df = set_fk_molecular_characterization(expression_df, 'expression', molecular_characterization_df)
     expression_df = harmonise_mutation_marker_symbols(expression_df, gene_markers_parquet_path)
     expression_df = get_expected_columns(expression_df)
     expression_df = add_id(expression_df, "id")
@@ -60,28 +61,6 @@ def get_expression_df(raw_expression_df: DataFrame) -> DataFrame:
         "ensembl_gene_id",
         "ncbi_gene_id",
         Constants.DATA_SOURCE_COLUMN,).drop_duplicates()
-
-
-def set_fk_molecular_characterization(expression_df: DataFrame, molecular_characterization_df: DataFrame) -> DataFrame:
-    expression_df = expression_df.withColumnRenamed("platform_id", "platform_external_id")
-    molecular_characterization_df = molecular_characterization_df.withColumnRenamed(
-        "id", "molecular_characterization_id").where("molecular_characterisation_type = 'expression'")
-
-    molecular_characterization_df = molecular_characterization_df.select(
-        "molecular_characterization_id", "sample_origin", "external_patient_sample_id",
-        "external_xenograft_sample_id", "platform_external_id", Constants.DATA_SOURCE_COLUMN)
-
-    mol_char_patient_df = molecular_characterization_df.where("sample_origin = 'patient'")
-    mol_char_patient_df = mol_char_patient_df.withColumnRenamed("external_patient_sample_id", "sample_id")
-    expression_patient_sample_df = expression_df .join(
-        mol_char_patient_df, on=["sample_id", "platform_external_id", Constants.DATA_SOURCE_COLUMN], how='inner')
-
-    mol_char_xenograft_df = molecular_characterization_df.where("sample_origin = 'xenograft'")
-    mol_char_xenograft_df = mol_char_xenograft_df.withColumnRenamed("external_xenograft_sample_id", "sample_id")
-    expression_xenograft_sample_df = expression_df.join(
-        mol_char_xenograft_df, on=["sample_id", "platform_external_id", Constants.DATA_SOURCE_COLUMN], how='inner')
-    expression_df = expression_patient_sample_df.union(expression_xenograft_sample_df)
-    return expression_df
 
 
 def get_expected_columns(expression_df: DataFrame) -> DataFrame:
