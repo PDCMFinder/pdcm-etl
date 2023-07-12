33280ed64b9f490375ef55bd5d287f3ef85a6eac
Mauricio Martinez Jimenez <mmartinez@ebi.ac.uk>
2023-07-12 14:29:00 +0100

    Debug info for yaml reader

--- etl/conf_file_readers/yaml_config_file_reader.py
+++ etl/conf_file_readers/yaml_config_file_reader.py
@@ -1,3 +1,4 @@
+import os
 from functools import lru_cache
 from pathlib import Path
 
@@ -6,6 +7,14 @@ import yaml
 
 @lru_cache(maxsize=None)
 def read_yaml_config_file(file_name):
+    print("Debug info for read_yaml_config_file [", file_name, "]")
+    cwd = os.getcwd()
+    print("cwd", cwd)
+    op1 = "etl/" + file_name
+    op2 = "../etl/" + file_name
+    print("original:", file_name, ".Path?", Path(file_name).is_file())
+    print("op1:", op1, ".Path?", Path(op1).is_file())
+    print("op2:", op2, ".Path?", Path(op2).is_file())
     file_path = file_name
     file_path_with_root_folder = "etl/" + file_path
     if Path(file_path_with_root_folder).is_file():
