333fbd631a4f73ecdad01da6dfb6c1af2817a52e
Mauricio Martinez Jimenez <mmartinez@ebi.ac.uk>
2024-04-08 14:54:40 +0100

    Fixing failing tests

--- tests/etl/workflow/treatment_component/input_data.py
+++ tests/etl/workflow/treatment_component/input_data.py
@@ -2,22 +2,26 @@ treatment = [
     {
         "id": "1",
         "name": "Dactolisib",
-        "data_source": "TRACE"
+        "data_source": "TRACE",
+        "type": "Not provided"
     },
     {
         "id": "2",
         "name": "capecitabine",
-        "data_source": "TRACE"
+        "data_source": "TRACE",
+        "type": "Not provided"
     },
     {
         "id": "3",
         "name": "PREDNISONE",
-        "data_source": "TRACE"
+        "data_source": "TRACE",
+        "type": "Not provided"
     },
     {
         "id": "4",
         "name": "FU-LV regimen",
-        "data_source": "TRACE"
+        "data_source": "TRACE",
+        "type": "Not provided"
     }
 ]
 
@@ -27,27 +31,35 @@ treatment_and_component_helper = [
         "model_id": "1",
         "patient_id": None,
         "treatment_name": "Dactolisib",
-        "treatment_dose": "40.0mg/kg"
+        "treatment_dose": "40.0mg/kg",
+        "treatment_type": "Not provided",
+        "data_source": "TRACE"
     },
     {
         "treatment_protocol_id": "2",
         "model_id": None,
         "patient_id": "1",
         "treatment_name": "capecitabine",
-        "treatment_dose": "500 mg/m2"
+        "treatment_dose": "500 mg/m2",
+        "treatment_type": "Not provided",
+        "data_source": "TRACE"
     },
     {
         "treatment_protocol_id": "3",
         "model_id": None,
         "patient_id": "1",
         "treatment_name": "PREDNISONE",
-        "treatment_dose": "500 mg/m2"
+        "treatment_dose": "500 mg/m2",
+        "treatment_type": "Not provided",
+        "data_source": "TRACE"
     },
     {
         "treatment_protocol_id": "4",
         "model_id": None,
         "patient_id": "1",
         "treatment_name": "   FU-LV regimen   ",
-        "treatment_dose": "500 mg/m2"
+        "treatment_dose": "500 mg/m2",
+        "treatment_type": "Not provided",
+        "data_source": "TRACE"
     }
 ]
