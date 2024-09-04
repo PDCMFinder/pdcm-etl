21cbf37cd3ecdb09d13b5215187cae83a0ba9175
Mauricio Martinez Jimenez <mmartinez@ebi.ac.uk>
2024-09-04 16:23:21 +0100

    Adds PubChem links

--- etl/jobs/transformation/links_generation/treatments_links_builder.py
+++ etl/jobs/transformation/links_generation/treatments_links_builder.py
@@ -46,20 +46,26 @@ def add_treatment_links(treatment_df: DataFrame, resources_df: DataFrame):
             tmp_df = find_chembl_links(unique_treatment_names_df, resource)
             all_links_df = all_links_df.unionAll(tmp_df)
 
-        elif  resource["link_building_method"] == "PubChemInlineLink":
-            print("Create links for PubChem (NOT IMPLEMENTED YET)")
+        elif resource["link_building_method"] == "PubChemInlineLink":
+            print("Create links for PubChem")
+            tmp_df = find_pubchem_links(unique_treatment_names_df, resource)
+            all_links_df = all_links_df.unionAll(tmp_df)
 
     treatment_names_links_column_df = create_treatment_links_column(all_links_df)
 
     # # Join back to the original data frame to add the new column to it
-    treatment_df = treatment_df.join(treatment_names_links_column_df, on=["name"], how="left")
+    treatment_df = treatment_df.join(
+        treatment_names_links_column_df, on=["name"], how="left"
+    )
 
     return treatment_df
 
 
 def find_chembl_links(treatment_names_df: DataFrame, resource) -> DataFrame:
     get_chembl_id_udf = udf(get_chembl_id, StringType())
-    treatment_names_df = treatment_names_df.withColumn("chembl_id", get_chembl_id_udf("name"))
+    treatment_names_df = treatment_names_df.withColumn(
+        "chembl_id", get_chembl_id_udf("name")
+    )
 
     data_df = treatment_names_df.withColumn("resource_label", lit(resource["label"]))
     links_df = data_df.withColumn("link", lit(resource["link_template"]))
@@ -122,9 +128,52 @@ def find_chembl_id_by_synonym(input: str) -> str:
     return chembl_id
 
 
+def find_pubchem_links(treatment_names_df: DataFrame, resource) -> DataFrame:
+    get_pubchem_id_udf = udf(get_pubchem_id, StringType())
+    treatment_names_df = treatment_names_df.withColumn(
+        "pubchem_id", get_pubchem_id_udf("name")
+    )
+
+    data_df = treatment_names_df.withColumn("resource_label", lit(resource["label"]))
+    links_df = data_df.withColumn("link", lit(resource["link_template"]))
+
+    links_df = links_df.withColumn(
+        "link",
+        when(col("pubchem_id").isNull(), None).otherwise(
+            expr("regexp_replace(link, 'PubChem_ID', pubchem_id)")
+        ),
+    )
+
+    return links_df.select("name", "resource_label", "link")
+
+
+# Tries to find the PubChem id for the treatment name. For now no search by synonyms
+def get_pubchem_id(treatment_name: str) -> str:
+    pubchem_id = find_pubchem_id_by_name(treatment_name)
+    print(f"By name {treatment_name} is {pubchem_id}")
+    return pubchem_id
+
+
+def find_pubchem_id_by_name(input: str) -> str:
+    pubchem_id = None
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{input}/cids/TXT"
+    response = requests.get(url)
+
+    if response.status_code != 404:
+        pubchem_id: str = response.text
+        pubchem_id = pubchem_id.replace('\n','')
+        pubchem_id = pubchem_id.replace('\t','')
+
+    return pubchem_id
+
+
 # Takes a df with the columns <"name", "resource_label", "link"> and returns a df
 # with columns <"name", "external_db_links"> where "treatment_links" is a JSON with the information to build links in the UI
 def create_treatment_links_column(links_df: DataFrame) -> DataFrame:
+
+    # Only interested in cases where links where found. This filter here causes `external_db_links` to br null if no links found.
+    links_df = links_df.where("link is not null")
+
     links_json_entry_column_df = links_df.withColumn(
         "json_entry", to_json(struct("resource_label", "link"))
     )
