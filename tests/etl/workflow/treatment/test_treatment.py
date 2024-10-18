b8deec5a0276a505af6f62855e12391bc889f559
Mauricio Martinez Jimenez <mmartinez@ebi.ac.uk>
2024-10-18 13:41:37 +0100

    Fixes repeated links in treatments

--- tests/etl/workflow/treatment/test_treatment.py
+++ tests/etl/workflow/treatment/test_treatment.py
@@ -1,7 +1,5 @@
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import to_json, trim, when, col, regexp_replace
-import json
+from pyspark.sql.functions import when, col, regexp_replace
 from etl.jobs.transformation.treatment_transformer_job import transform_treatment
 from tests.etl.workflow.treatment.expected_outputs import expected_treatments
 from tests.etl.workflow.treatment.input_data import (
@@ -12,7 +10,6 @@ from tests.util import convert_to_dataframe, assert_df_are_equal_ignore_id
 
 
 def test_treatment(spark_session):
-    spark = SparkSession.builder.getOrCreate()
     treatment_type_helper_df: DataFrame = convert_to_dataframe(
         spark_session, treatment_type_helper
     )
@@ -23,10 +20,10 @@ def test_treatment(spark_session):
         treatment_type_helper_df, raw_external_resources_df
     )
     expected_df = convert_to_dataframe(spark_session, expected_treatments)
+    treatment_df.show()
+    expected_df.show()
 
-
-
-#    Normalise external_db_links to help in the comparison. Without this, the string contains some spaces that don't match the real result
+    # Normalise external_db_links to help in the comparison. Without this, the string contains some spaces that don't match the real result
     expected_normalized_df = expected_df.withColumn(
         "external_db_links",
         when(
